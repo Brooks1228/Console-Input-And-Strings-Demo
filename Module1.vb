551239b1933cb60fc80db7409703e51a8a817f99
brooks !! <144816371+Brooks1228@users.noreply.github.com>
2023-09-20 10:08:15 -0400

    Add project files.

---
+++ Module1.vb
@@ -0,0 +1,45 @@
+﻿Module Module1
+    'Brooks Keller
+    'String variables and input demo
+    '9/20/23
+    Sub Main()
+        Dim name As String 'declare a variable to hold text
+        Dim movieTitle1 As String
+        Dim movieTitle2 As String
+        Dim movieTitle3 As String = "Trolls World Tour" 'you cna initialize (give a value) to a variable when you create it
+        Dim movieYear1, movieYear2 As String 'you can declare multiple variables of the same type on one line
+        Dim movieYear3 = "2020"
+
+        Console.Write("please enter your name -> ")
+        name = Console.ReadLine() 'read from the console everything until enter key hit
+        Console.WriteLine("hello " & name & "!!") 'concatonate the string literal (i added my own exclamation points for expression)
+
+        Console.Write("please enter your first favorite movie of all time -> ") 'prompt user for input
+        movieTitle1 = Console.ReadLine
+        Console.Write("please enter the year it came out -> ")
+        movieYear1 = Console.ReadLine()
+
+        Console.Write("please enter your second favorite movie of all time -> ")
+        movieTitle2 = Console.ReadLine
+        Console.Write("please enter the year it came out -> ")
+        movieYear2 = Console.ReadLine()
+
+        'print the string. escape the quotes with an additional "
+        Console.WriteLine("thank you! your top ""top 3"" movies of all time are listed below" & vbNewLine)
+
+        'print a line of dashes for the top of the table
+        Console.WriteLine("".PadRight(56, "-"))
+        'uses a string formatter
+        Console.WriteLine("| {0} | {1} | {2} |", "#", "title".PadRight(30), "year released".PadRight(15))
+        'the above is the same as the below, which uses concatenation
+        'Console.Writeline("| # | " & "title".padright(30) & " | " & "year released".padright (15) & " | ")
+        Console.WriteLine("".PadRight(56, "-"))
+        Console.WriteLine("| {0} | {1} | {2} |", "1", movieTitle1.PadRight(30), movieYear1.PadRight(15))
+        Console.WriteLine("| {0} | {1} | {2} |", "2", movieTitle2.PadRight(30), movieYear2.PadRight(15))
+        Console.WriteLine("| {0} | {1} | {2} |", "3", movieTitle3.PadRight(30), movieYear3.PadRight(15))
+        Console.WriteLine("".PadRight(56, "-"))
+
+        Console.ReadKey()
+    End Sub
+
+End Module
---
+++ stringDemo2.vb
@@ -0,0 +1,39 @@
+﻿Module stringDemo2
+    'Brooks Keller
+    'more string functions
+    '9/20/23
+    Sub main()
+        Dim name, input1, input2, input3 As String
+        Console.Write("please enter your name -> ")
+        name = Console.ReadLine() 'read from the console everything until enter key hit
+        Console.WriteLine("hello " & name & "!!") 'concatonate the string literal (i added my own exclamation points for expression)
+        Console.WriteLine()
+
+        Console.WriteLine("lets play a game where you say something and i ""shout"" it back!")
+        Console.Write("what should i ""shout""? -> ")
+        input1 = Console.ReadLine
+        Console.WriteLine(input1.ToUpper) 'the ToUpper method is a string function to make all text caps
+        Console.WriteLine()
+
+        Console.WriteLine("that was fun. lets do it again!")
+        Console.Write("now, what should i ""shout""? -> ")
+        input2 = Console.ReadLine
+        Console.WriteLine(input2.ToUpper)
+        Console.WriteLine()
+
+        Console.WriteLine("i like this! lets do it again")
+        Console.Write("one more time, what should i ""shout""? -> ")
+        input3 = Console.ReadLine
+        Console.WriteLine(input3.ToUpper)
+        Console.WriteLine()
+
+        Console.WriteLine("oh that was fun! let me try ""whispering"" all of back now")
+        Console.WriteLine(input1.ToLower) 'the ToLower method is a string function to make all text lowercase
+        Console.WriteLine(input2.ToLower)
+        Console.WriteLine(input3.ToLower)
+
+        Console.WriteLine()
+        Console.WriteLine("Thanks for the fun...Have a great day!!")
+        Console.ReadKey()
+    End Sub
+End Module
